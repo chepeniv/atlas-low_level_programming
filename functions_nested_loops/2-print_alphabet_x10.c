b9636f94d1c79c4bff3b2543dd993bc924b1b4bb
chepeniv <jose.olmos@atlasschool.com>
2024-05-17 22:41:45 +0000

    S3-P3-T2 moved print_alphabet() into 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * print_alphabet - printout lowercase alphabet
+ *
+ * Return: returns void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97 ; i <= 122 ; ++i)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
+
+/**
  * print_alphabet_x10 - calls print_alphabet() ten times
  *
  * Return: returns void
