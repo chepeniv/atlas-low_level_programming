45136fc16d1e27f8ca3760229b8d0a4c3405dc70
chepeniv <jose.olmos@atlasschool.com>
2024-05-17 14:28:33 +0000

    S3-P3-T0 successful compile and exection of c script

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printout the string _putchar
+ *
+ * Return: returns 0 upon successful execution
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
