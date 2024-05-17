4c4e30897bd9ca358a42e09155bf30c1cfaaa89e
chepeniv <jose.olmos@atlasschool.com>
2024-05-17 23:39:24 +0000

    S3-P3-t5 wrote working c source

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - determines whether a given integer is positive, negative, or zero
+ * @n: the int to be evaluated
+ *
+ * Return: returns 1 for positive, -1 for negative, and 0 for zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else 
+	{
+		_putchar('0');
+		return (0);
+	}
+}
