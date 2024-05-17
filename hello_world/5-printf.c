81b4f80b2aaae842a578ee64c05c7afad1c1775c
chepeniv <jose.olmos@atlasschool.com>
2024-05-17 07:23:20 +0000

    P1T5: created working c prog

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a predefined string to stdout
+ *
+ * Return: returns 0 upon successful execution
+ */
+int main(void)
+{
+	const char s[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", s);
+	return (0);
+}
