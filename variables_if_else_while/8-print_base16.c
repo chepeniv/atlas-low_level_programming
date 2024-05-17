9e65fd937703552e76ee9bf07d9197e54af91981
chepeniv <jose.olmos@atlasschool.com>
2024-05-17 12:24:27 +0000

    S3-P2-T8: successful compilation and run from c source

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - printout all the base 16 digits 
+ *
+ * Return: returns 0 upon successful execution
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= 97+5; ++i)
+	{
+		putchar(i);
+	}
+
+	putchar(10);
+
+	return (0);
+}
