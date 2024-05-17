5d1c6766516c3130ba19c1f438ab91de7c425587
chepeniv <jose.olmos@atlasschool.com>
2024-05-17 11:22:30 +0000

    P2-T1: experimenting with long line break style

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -30,8 +30,10 @@ int main(void)
 		else
 		{
 			printf(
-				"Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit
-				);
+				"Last digit of %d is %d and is less than 6 and not 0\n", 
+				n, 
+				last_digit
+			);
 		}
 	}
 
