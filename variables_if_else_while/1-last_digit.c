befcc9a096060899ffc85618c4c51be1bef7e5ea
chepeniv <jose.olmos@atlasschool.com>
2024-05-17 11:21:08 +0000

    P2-T1: removed trailing whitespace. attempt to shorten long lines

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,8 +14,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	last_digit = n % 10; 
+
+	last_digit = n % 10;
 
 	if (last_digit >= 6)
 	{
@@ -29,7 +29,9 @@ int main(void)
 		}
 		else
 		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+			printf(
+				"Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit
+				);
 		}
 	}
 
