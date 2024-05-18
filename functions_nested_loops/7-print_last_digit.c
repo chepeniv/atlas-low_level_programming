db4ff3845501d261e6c685e9cc5ba4b84513ccd3
chepeniv <jose.olmos@atlasschool.com>
2024-05-18 02:34:36 +0000

    S3-P3-T7 got working code on my sandbox

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+//#include <stdio.h>
+
+/**
+ * print_last_digit - evaluates what the final digit of a given integer is
+ * @n: the int to be evaluated
+ *
+ * Return: int value of final digit
+ */
+int print_last_digit(int n)
+{
+	int l = (n % 10) + 48; 
+	_putchar(l);
+	return (n);
+}
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -5,14 +5,16 @@ int main(void)
 {
     int r;
 
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
+    print_last_digit(98);
+    print_last_digit(0);
+    print_last_digit(487539);
+    print_last_digit(34854757);
+    print_last_digit(475656);
+    print_last_digit(43355);
+    print_last_digit(34477);
+    _putchar(10);
+    //r = print_last_digit(-1024);
+    //_putchar('0' + r);
 
 	return (0);
 }
