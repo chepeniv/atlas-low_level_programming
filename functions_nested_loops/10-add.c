48133bf75a867ef5868203a643688f93aeff7763
chepeniv <jose.olmos@atlasschool.com>
2024-05-19 09:56:14 +0000

    S3-P3 saving some mathematical tinkering

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
-void pop_last(int);
+/*void print_rev(int);*/
+void print_fwrd(int);
+int get_first(int);
+int _exp(int, int);
+
 /**
  * print_to_98 - count from any given number towards 98
  * @n: the number to count from
@@ -29,20 +33,24 @@ void print_to_98(/*int n*/)
 	}
 */
 	int aa = 4857577;
-	int bb = 3838734;
-	int cc = 3884432;
+	int bb = 383873;
+	int cc = 23767;
+	int dd = 1344;
 
 	printf("testing for number %d\n", aa);
-	print_rev(aa);
+	print_fwrd(aa);
 	_putchar(10);
 	printf("testing for number %d\n", bb);
-	print_rev(bb);
+	print_fwrd(bb);
 	_putchar(10);
 	printf("testing for number %d\n", cc);
-	print_rev(cc);
+	print_fwrd(cc);
+	_putchar(10);
+	printf("testing for number %d\n", dd);
+	print_fwrd(dd);
 	_putchar(10);
 }
-
+/*
 void print_rev(int m)
 {
 	while (m != 0) 
@@ -51,4 +59,43 @@ void print_rev(int m)
 		_putchar(l + '0');
 		m = (m - l) / 10;
 	}
+}*/
+
+void print_fwrd(int m)
+{
+	while (m > 1) 
+	{
+		int f = m;
+		int e = 0;
+
+		while (f >= 10)
+		{
+			f /= 10;
+			e += 1;
+		}
+		_putchar(f + '0');
+		m = m - (f * _exp(10, e));
+	}
+
 }
+
+int _exp(int base, int exp)
+{
+	int pwr = base;
+
+	while (exp > 1)
+	{
+		pwr = pwr * base;
+		--exp;
+	}
+	return (pwr);
+}
+/*
+int get_first(int f)
+{
+	while (f >= 10)
+	{
+		f /= 10;
+	}
+	return (f);
+}*/
