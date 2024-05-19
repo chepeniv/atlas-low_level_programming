2b344c4672726107c5e08909f7724e57fb1e1fe8
chepeniv <jose.olmos@atlasschool.com>
2024-05-19 05:28:51 +0000

    S3-P3-T10 conformed to betty style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,12 +3,13 @@
 /**
  * add - calculates the sum of two given ints
  * @a: the first int
- * @b: the second int 
+ * @b: the second int
  *
  * Return: the sum of a and b
  */
 int add(int a, int b)
 {
 	int c = a + b;
+
 	return (c);
 }
