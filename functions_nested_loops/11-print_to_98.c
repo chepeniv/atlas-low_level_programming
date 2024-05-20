83e7b314632956f975c52852c44eeb23bc9875c9
chepeniv <jose.olmos@atlasschool.com>
2024-05-20 12:05:40 +0000

    S3-P3-T11 cleaned up and focused sourcecode

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 
-int _exp(int, int);
-void print_fwrd(int);
-int rev_int(int);
-int reduce_int(int n);
+void into_string(char str[32], int n)
+{
+        int i = 30;
+        while (n !=0)
+        {
+                str[i--] = (n % 10) + '0';
+                n /= 10;
+        }
+}
+
+void print_string(char str[32])
+{
+	int size = sizeof(*str);
+	int i = 0;
+
+	while (i < size)
+	{
+		_putchar(str[i]);
+		++i;
+	}
+}
+
+void print_int(int n)
+{
+	char str[32];
+
+	into_string(str, n);
+	print_string(str);
+}
 
 /**
  * print_to_98 - count from any given number towards 98
@@ -14,14 +38,13 @@ int reduce_int(int n);
  */
 void print_to_98(int n)
 {
-	/*
 	int i;
 
 	if (n > 98)
 	{
 		for (i = n; i > 98; i--)
 		{
-			print_fwrd(i);
+			print_int(i);
 			_putchar(44);
 			_putchar(32);
 		}
@@ -30,101 +53,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			print_fwrd(i);
+			print_int(i);
 			_putchar(44);
 			_putchar(32);
 		}
 	}
-	print_fwrd(98);
+	print_int(98);
 	_putchar(10);
 	_putchar(10);
-	*/
-
-	printf("original : %d\n", 10);
-	printf("reversed : %d\n", rev_int(10));
-	printf("original : %d\n", 2301);
-	printf("reversed : %d\n", rev_int(2301));
-	printf("original : %d\n", 13);
-	printf("reversed : %d\n", rev_int(13));
-	printf("original : %d\n", 670374);
-	printf("reversed : %d\n", rev_int(670374));
-	printf("original : %d\n", 4704420);
-	printf("reversed : %d\n", rev_int(4704420));
-	printf("original : %d\n", 662);
-	printf("reversed : %d\n", rev_int(662));
 }
-
-void print_fwrd(int m)
-{
-	while (m > 1) 
-	{
-		int f = m;
-		int e = 0;
-
-		while (f >= 10)
-		{
-			f /= 10;
-			e += 1;
-		}
-
-		_putchar(f + '0');
-
-		m = m - (f * _exp(10, e));
-
-	}
-}
-
-int reduce_int(int n)
-{
- 	int last = n % 10;
- 	n = (n - last) / 10;
-	return (n);
-}
-
-int _exp(int base, int exp)
-{
-	int pwr = base;
-
-	while (exp > 1)
-	{
-		pwr = pwr * base;
-		--exp;
-	}
-	return (pwr);
-}
-
-int rev_int(int n)
-{
-	int modulo; 
-	int place = 0; 
-	int rsum = 0; 
-
-	while (n > 0)
-	{
-		modulo = n % 10;
-		place++;
-
-		if (modulo == 0)
-		{
-			rsum = rsum * 10;
-		}
-		else
-		{
-			rsum = rsum + (modulo * _exp(10, place));
-		}
-
-		n = reduce_int(n);
-	}
-	return (rsum);
-}
-
-/*void print_rev(int m)
- *{
- *	while (m != 0) 
- *	{
- *		int l = m % 10;
- *		_putchar(l + '0');
- *		m = (m - l) / 10;
- *	}
- *}
- */
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-    int r;
-/*
-    print_last_digit(INT_MIN);
-    printf("--%d\n", INT_MIN);
-*/
     	print_to_98(140);
+    	print_to_98(30);
+    	print_to_98(70);
+    	print_to_98(130);
+    	print_to_98(98);
+    	print_to_98(0);
 	return (0);
 }
