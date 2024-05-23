dc3a06c6ab24b16c8c48cbefbc18af3a84a9ec8e
chepeniv <jose.olmos@atlasschool.com>
2024-05-23 01:30:25 +0000

    S4-P1-T2 first attempt

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * _mul - multiply two given integers and return their product
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the product of a and b
+ */
+int _mul(int a, int b)
+{
+	return (a * b);
+}
