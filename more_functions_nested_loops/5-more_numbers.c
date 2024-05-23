951445f10dcaa726a5cc4257010511ec8c3c3901
chepeniv <jose.olmos@atlasschool.com>
2024-05-23 02:14:44 +0000

    S4-P1-T5 successful sandbox compilation and run

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_int - handle the printout of a two-digit integer greater than or equal
+ * to zero
+ *
+ * Return: void
+ */
+void print_int(int n)
+{
+	if (n > 9)
+		_putchar(n / 10 + '0');
+
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * more_numbers - printout out integers 0 through 14 inclusive, ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int num = 0;
+	int line = 1;
+
+	while (line <= 10)
+	{
+		while (num <= 14)
+		{
+			print_int(num);
+			num++;
+		}
+		_putchar('\n');
+
+		num = 0; 
+		line++;
+	}
+}
--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -4,6 +4,6 @@
 /* testing task's sourcecode locally */
 int main(void) 
 {
-	print_most_numbers();
+	more_numbers();
 	return (0);
 }
