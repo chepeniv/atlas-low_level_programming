c27830c45bbfbed0fdeeb3e3c985b9d99445009e
chepeniv <jose.olmos@atlasschool.com>
2024-05-23 04:06:30 +0000

    S4-P1-T9 successful sandbox compilation and run

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printout a square with sides of given length
+ *
+ * Return: void
+ */
+void main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0 )
+			printf("%d", i);
+		printf(" ");
+	}
+}
