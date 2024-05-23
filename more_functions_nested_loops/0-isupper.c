0b22e579b46345ea3f46b812a8fc09b380cab34b
chepeniv <jose.olmos@atlasschool.com>
2024-05-23 01:12:11 +0000

    S4-P1-T0 added betty doc and conformed to betty style

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,17 @@
+/**
+ * _isupper - test whether a given char is an uppercase letter
+ * @c: the char to test
+ *
+ * Return - 1 for uppercase letter, 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
