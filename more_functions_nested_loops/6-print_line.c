ebae8d0857aca3e7d130622894faeb55294a65dc
chepeniv <jose.olmos@atlasschool.com>
2024-05-23 02:27:36 +0000

    S4-P1-T6 successful sandbox compilation and run

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - printout a line of a specified length
+ * @length: the length of the line
+ *
+ * Return: void
+ */
+void print_line(int length)
+{
+	if (length > 0)
+	{
+		int position = 1;
+		while (position <= length)
+		{
+			_putchar('_');
+			position++;
+		}
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -4,6 +4,13 @@
 /* testing task's sourcecode locally */
 int main(void) 
 {
-	more_numbers();
+	//more_numbers();
+	print_line(-10);
+	print_line(0);
+	print_line(2);
+	print_line(4);
+	print_line(8);
+	print_line(16);
+	print_line(32);
 	return (0);
 }
