cc6f3d8fe40cfcc6bdf8b2bd13a466613078ee16
chepeniv <jose.olmos@atlasschool.com>
2024-05-23 04:21:23 +0000

    S4-P1-T9 conformed to betty style and edited doc desc

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - printout a square with sides of given length
+ * main - classic FizzBuzz challenge
  *
  * Return: 0 upon completion
  */
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 			printf("Fizz");
 		if (i % 5 == 0)
 			printf("Buzz");
-		if (i % 3 != 0 && i % 5 != 0 )
+		if (i % 3 != 0 && i % 5 != 0)
 			printf("%d", i);
 		if (i < 100)
 		{
