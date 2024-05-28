8b8c29551b5dc8115d248ff46d4cf16e9ead3483
chepeniv <jose.olmos@atlasschool.com>
2024-05-28 02:16:45 +0000

    S3-P2-T10 thought of a cleaner way to handle limit cases

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,6 +3,18 @@
 #include <limits.h>
 
 /**
+ * countDigits - calculate the number of digits in a base ten number
+ * @number: int to examine
+ *
+ * Return: int denoting the number of digits in 'number'
+ */
+int countDigits(int number)
+{
+	if (number < 10) return 1;
+	return 1 + countDigits(number / 10);
+}
+
+/**
  * _atoi - construct an integer from a given string
  * @string: pointer to string
  *
@@ -13,6 +25,8 @@ int _atoi(char *string)
 	int number = 0;
 	int position = 0;
 	int sign = 1;
+	int digits = 0;
+	int maxDigits = countDigits(INT_MAX);
 	char character;
 
 	while (string[position] != '\0')
@@ -27,15 +41,23 @@ int _atoi(char *string)
 			character = string[++position];
 			while (character >= '0' && character <= '9')
 			{
-				number *= 10;
-				number += (int)character - 48;
-				character = string[++position];
+				if (digits < maxDigits)
+				{
+					number *= 10;
+					number += (int)character - 48;
+					character = string[++position];
+				}
+				else
+				{
+					number = INT_MAX;
+				}
 			}
 			break;
 		}
 		position++;
 	}
 
-	number *= sign;
+	if (sign < 0 && number == INT_MAX) return INT_MIN;
+	else number *= sign;
 	return (number);
 }
