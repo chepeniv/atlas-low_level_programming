a0cc6f4f05ee0f12446127081e2c1770332291fc
chepeniv <jose.olmos@atlasschool.com>
2024-05-28 02:29:57 +0000

    S3-P2-T10 cleaned up unused variables and functions

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,18 +3,6 @@
 #include <limits.h>
 
 /**
- * countDigits - calculate the number of digits in a base ten number
- * @number: int to examine
- *
- * Return: int denoting the number of digits in 'number'
- */
-int countDigits(int number)
-{
-	if (number < 10) return 1;
-	return 1 + countDigits(number / 10);
-}
-
-/**
  * _atoi - construct an integer from a given string
  * @string: pointer to string
  *
@@ -22,12 +10,10 @@ int countDigits(int number)
  */
 int _atoi(char *string)
 {
+	long number = 0;
 	int position = 0;
 	int sign = 1;
-	int digits = 0;
 	int final = 0;
-	int maxDigits = countDigits(INT_MAX);
-	long number = 0;
 	char character;
 
 	while (string[position] != '\0')
