3079ebc6e41a822a516377118c25867acc3967a5
chepeniv <jose.olmos@atlasschool.com>
2024-05-30 08:27:15 +0000

    P1-T4 sandbox untested - 1st attempt

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the contents of an array of integers
+ * @num_array: the int array to operate on
+ * @length: the number of elements in num_array
+ *
+ * Return: void
+ */
+void reverse_array(int *num_array, int length)
+{
+	int hold;
+	int pos = 0;
+	int halfway = length / 2;
+
+	while (pos < halfway)
+	{
+		hold = num_array[pos];
+		num_array[pos] = num_array[length - pos];
+		num_array[length - pos] = hold;
+		pos++;
+	}
+}
