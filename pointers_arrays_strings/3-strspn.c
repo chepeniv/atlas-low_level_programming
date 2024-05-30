842f61cef5bb803596d8de43e37561bdf606d208
chepeniv <jose.olmos@atlasschool.com>
2024-05-30 15:13:06 +0000

    P2-T3 misunderstood task - rewrote logic to suit task's goals

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,35 @@
 #include "main.h"
 
 /**
- * _strspn - find the length of a prefix substring wherein a set of given
- * characters occur
- * @check_str: the string to search through
- * @search_set: the string containing the char's to search for
+ * _strspn - find the length of the prefix substring wherein _only_ a given set
+ * of characters occur
+ * @check_str: the string to analyse through
+ * @accept_set: the string containing chars that will be accepted
  *
- * Return: an unsigned int denoting the length of the substring containg at 
- * least one instance of each element of search_set
+ * Return: an unsigned int denoting the length of the substring containing only
+ * char's in the accept-set
  */
-unsigned int _strspn(char *check_str, char *search_set)
+unsigned int _strspn(char *check_str, char *accept_set)
 {
 	unsigned int a = 0;
 	unsigned int b = 0;
 	unsigned int size = 0;
 
-	while (search_set[a] != '\0')
+	while (check_str[a] != '\0')
 	{
-		while (check_str[b] != '\0')
+		while (accept_set[b] != '\0')
 		{
-			if (search_set[a] == check_str[b])
+			if (accept_set[a] == check_str[b])
 			{
-				if (size <= a)
-					size = a + 1; 
+				size++; 
 				break;
 			}
 			b++;
 		}
+
+		if (accept_set[a] == '\0')
+			break;
+
 		b = 0;
 		a++;
 	}
