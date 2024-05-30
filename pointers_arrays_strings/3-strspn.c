51886f92c986a5d02569fe4d031d01bbc4801126
chepeniv <jose.olmos@atlasschool.com>
2024-05-30 10:54:19 +0000

    P2-T3 saving work

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,28 +3,31 @@
 /**
  * _strspn - find the length of a prefix substring wherein a set of given
  * characters occur
- * @string: the string to search through
- * @search_set: the string containing the char to search for
+ * @check_str: the string to search through
+ * @search_set: the string containing the char's to search for
  *
  * Return: an unsigned int denoting the length of the substring containg at 
  * least one instance of each element of search_set
  */
-unsigned int _strspn(char *string, char *search_set);
+unsigned int _strspn(char *check_str, char *search_set)
 {
-	int set_i = 0;
-	int str_i = 0;
-	int size = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int size = 0;
 
-	while (search_set[set_i] != '\0')
+	while (check_str[i] != '\0')
 	{
-		while (string[str_i] != '\0')
-			if 
-		int str_i = 0;
-		index++;
+		while (search_set[j] != '\0')
+		{
+			if (check_str[i] == search_set[j])
+			{
+				size = i + 1; 
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
 	}
-
-	if (string[index] == csearch)
-		return (&string[index]);
-	else
-		return (NULL);
+	return (size);
 }
