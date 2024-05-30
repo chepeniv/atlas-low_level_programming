bdbf02b526b1db848c0d461ce4ddd88d16212f3c
chepeniv <jose.olmos@atlasschool.com>
2024-05-30 09:00:16 +0000

    P1-T5 sandbox-untested

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lowercase letters in a string to uppercase
+ * @string: the string to operate on
+ *
+ * Return: pointer to string
+ */
+void reverse_array(int *num_array, int length)
+{
+	int pos = 0;
+
+	while (string[i] != '\0')
+	{
+		if (string[i] >= 'a' && string[i] <= 'z')
+			string[i] -= 32;
+		pos++;
+	}
+}
