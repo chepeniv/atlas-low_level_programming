231d7abfce15e59564d992701f4143ef030f5a34
chepeniv <jose.olmos@atlasschool.com>
2024-05-31 11:57:29 +0000

    P1-T7 saving logic buildup

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -14,45 +14,25 @@
  */
 char *leet(char *text)
 {
-	int t = 0;
-	int l = 0;
+	int t;
+	int l;
+	char leet[] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'}; 
 
-	char leet[8][2] = {
-		{'o', 'O'},
-		{'l', 'L'},
-		{'2', '2'},
-		{'e', 'E'},
-		{'a', 'A'},
-		{'5', '5'},
-		{'6', '6'},
-		{'t', 'T'}
-	}; // a = A + 32;
-
-	while (text[i] != '\0')
+	t = 0;
+	while (text[t] != '\0')
 	{
-		//while not the end of comparisons
-		//	set some value, a pointer ??
-		//	set an index ?? 
-		//if some value set 
-		//	operate ??
-		//	use an index to properly operate ?
-
-		if (
-		text[i] == 'a' || 
-		text[i] == 'A' ||
-		text[i] == 'e' || 
-		text[i] == 'E' ||
-		text[i] == 'o' ||
-		text[i] == 'O' ||
-		text[i] == 't' ||
-		text[i] == 'T' ||
-		text[i] == 'l' ||
-		text[i] == 'L'
-		)
+		l = 0
+		while (l < 8)
 		{
-
+			if (leet[l] != '\0' && 
+			(text[t] == leet[l] || text[t] == leet[l] + 32))
+			{
+				text[t] = l + '0';
+				break;
+			}
+			l++;
 		}
-		i++;
+		t++;
 	}
 
 	return (text);
