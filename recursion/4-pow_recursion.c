f9bb9fdd1b4450f08c2b6198c470289234f0f537
chepeniv <jose.olmos@atlasschool.com>
2024-06-06 17:44:09 +0000

    recursion-4 sandbox untested code

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate the product of `base` multiplied by itself 
+ * `power` number of times
+ * @base: integer to multiply
+ * @power: number of multiplications
+ *
+ * Return: int result
+ */
+int _pow_recursion(int base, int power)
+{
+	if (power < 0)
+		return (-1);
+	else if (power == 0)
+		return (1);
+	else
+		return (base * _pow_recursion(number - 1));
+}
