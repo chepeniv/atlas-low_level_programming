bebb54b3edb9c63561e6b2c3d649c3fafad9e577
chepeniv <jose.olmos@atlasschool.com>
2024-06-06 17:26:07 +0000

    recursion-3 sandbox untested code

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial product of a given integer
+ * @number: integer to calculate factorial for
+ *
+ * Return: int result
+ */
+int factorial(int number)
+{
+	if (number < 0)
+		return (-1);
+	else if (number <= 1)
+		return (1);
+	else
+		return (number * factorial(--number));
+}
