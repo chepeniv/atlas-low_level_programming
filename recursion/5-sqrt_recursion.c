7b04324465784be508bea6e79b30717fcf7d1307
chepeniv <jose.olmos@atlasschool.com>
2024-06-07 05:25:38 +0000

    recursion-5: aww, man. it looks like i cant do that in c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,6 @@
 #include "main.h"
 
-/*int rec_calc_sqrt(int guess, int target);*/
-
-int _sqrt_recursion(int guess, int target);
+int rec_calc_sqrt(int guess, int target);
 
 /**
  * _sqrt_recursion - calculate the natural square root of a number recursivly
@@ -17,25 +15,25 @@ int _sqrt_recursion(int num)
 	if (num < 0)
 		return (-1);
 	else
-		return (_sqrt_recursion(guess, num));
+		return (rec_calc_sqr(guess, num));
 }
 
 /**
- * _sqrt_recursion- handles the sqrt caculation for its parent by using two
+ * rec_calc_sqrt- handles the sqrt caculation for its parent by using two
  * params
  * @guess: used to calculate a square
  * @target: the value to attemp to match with a square
  *
  * Return: int result
  */
-int _sqrt_recursion(int guess, int target)
+int rec_calc_sqrt(int guess, int target);
 {
 	int square = guess * guess;
 
 	if (square == target)
 		return (guess);
 	else if (square < target)
-		return (_sqrt_recursion(++guess, target));
+		return (rec_calc_sqrt(++guess, target));
 	else
 		return (-1);
 }
