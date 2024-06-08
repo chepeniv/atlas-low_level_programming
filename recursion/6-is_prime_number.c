f8b51adb82c80da4ca7ed8163a740a5c26e943eb
chepeniv <jose.olmos@atlasschool.com>
2024-06-08 05:20:08 +0000

    incomplete - transforming iterative logic into recursive logic

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,20 +1,72 @@
 #include "main.h"
 
-int _calc_sqrt(int guess, int target);
+int _sqrt(int guess, int target);
+void init_array(int *array, int size, int pos);
+void find_multiples(int *nums, int factor, int stop);
+void det_primes(int *nums, int stop);
 
-int is_prime_number(int number)
+/**
+ * is_prime_number - determine if the given number is a prime
+ * @num: integer to investigate
+ *
+ * Return: an int value of 1 for prime or 0 for not prime
+ */
+int is_prime_number(int num)
 {
-	is prime number return 1;
-	is not prime number return 0;
-	sieve_of_eratos;
-	build an array initialized to all 1 that is the size of number;
-	calculate the upperbound of calculations using the sqrt of number;
-	iteratively markout nonprimes in the array;
-		stop and return if the array[number] position becomes 0;
-		or the upper sqrt bound is reached;
-			return the array[number] pos value;
+	int upperbound, factor, multiple, i;
+	int sieve[num + 1];
+
+	if (num <= 1 )
+		return 0;
+	else
+	{
+		upperbound = _sqrt(2, num);
+		factor = 2;
+		init_array(sieve, num, 0);
+	}
+
+	while (factor <= upperbound)
+	{
+		if (sieve[factor] == 1 )
+		{
+			multiple = factor + factor;
+			while (multiple <= num)
+			{
+				sieve[multiple] = 0;
+				multiple += factor;
+			}
+			if (sieve[num] == 0)
+				return 0;		
+		}
+		factor++;
+	}
+
+	return 1;
 }
 
+void find_multiples(int *nums, int factor, int stop)
+{
+
+}
+
+/**
+ * init_array - set all values of given array to 1
+ * @array: pointer to the array to operate on
+ * @max: determines how far in the array to work on
+ * @pos: the current working index 
+ *
+ * Return: void
+ */
+void init_array(int *array, int max, int pos)
+{
+	if (pos > max)
+		return;
+	else
+	{
+		*(array + pos) = 1;
+		init_array(array, max, pos++);
+	}
+}
 
 /**
  * _sqrt - calculate the natural square root of a number recursivly
@@ -22,33 +74,12 @@ int is_prime_number(int number)
  *
  * Return: int result
  */
-int _sqrt(int num)
-{
-	int guess = 0;
-
-	if (num < 0)
-		return (-1);
-	else
-		return (_calc_sqrt(guess, num));
-}
-
-/**
- * rec_calc_sqrt - handles the sqrt caculation for its parent by using two
- * params
- * @guess: used to calculate a square
- * @target: the value to attemp to match with a square
- *
- * Return: int result
- */
-int _calc_sqrt(int guess, int target)
+int _sqrt(int guess, int target)
 {
 	int square = guess * guess;
 
-	if (square == target)
+	if (square >= target)
 		return (guess);
-	else if (square < target)
-		return (_calc_sqrt(++guess, target));
 	else
-		return (-1);
+		return (_sqrt(++guess, target));
 }
-
