2a747625b18c92056929fdb2d08251d13b6fc41e
chepeniv <jose.olmos@atlasschool.com>
2024-06-08 07:34:01 +0000

    savepoint - gonna attemp another approach-

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,70 +1,66 @@
 #include "main.h"
+#include <stdio.h>
 
-int _sqrt(int guess, int target);
-void init_array(int *array, int size, int pos);
-void find_multiples(int *nums, int factor, int stop);
-void det_primes(int *nums, int stop);
-
-/**
- * is_prime_number - determine if the given number is a prime
- * @num: integer to investigate
- *
- * Return: an int value of 1 for prime or 0 for not prime
- */
-int is_prime_number(int num)
+int _sqrt(int target, int guess)
 {
-	int upperbound, factor, multiple, i;
-	int sieve[num + 1];
+	int square = guess * guess;
+	if (square >= target)
+		return (guess);
+	else
+		return (_sqrt(target, ++guess));
+}
 
-	if (num <= 1 )
-		return 0;
+void init_array(int array[], int target, int pos)
+{
+	if (pos > target)
+	{
+		return;
+	}
 	else
 	{
-		upperbound = _sqrt(2, num);
-		factor = 2;
-		init_array(sieve, num, 0);
+		array[pos] = 1;
+		init_array(array, target, ++pos);
 	}
-
-	while (factor <= upperbound)
-	{
-		if (sieve[factor] == 1 )
-		{
-			multiple = factor + factor;
-			while (multiple <= num)
-			{
-				sieve[multiple] = 0;
-				multiple += factor;
-			}
-			if (sieve[num] == 0)
-				return 0;		
-		}
-		factor++;
-	}
-
-	return 1;
 }
 
-void find_multiples(int *nums, int factor, int stop)
+void find_multiples(int sieve[], int target, int factor, int multiple)
 {
-
-}
-
-/**
- * init_array - set all values of given array to 1
- * @array: pointer to the array to operate on
- * @max: determines how far in the array to work on
- * @pos: the current working index 
- *
- * Return: void
- */
-void init_array(int *array, int max, int pos)
-{
-	if (pos > max)
+	if (multiple > target)
 		return;
 	else
 	{
-		*(array + pos) = 1;
-		init_array(array, max, pos++);
+		sieve[multiple] = 0;
+		multiple += factor;
+		find_multiples(sieve, target, factor, multiple);
+	}
+}
+
+int sift_primes(int sieve[], int target, int upperbound, int factor)
+{
+	if (factor > upperbound)
+		return sieve[target];
+	else if (sieve[factor] == 1)
+	{
+		find_multiples(sieve, target, factor, factor * 2);
+		if (sieve[target] == 0)
+			return 0;		
+	}
+	else
+		return (sift_primes(sieve, target, upperbound, ++factor));
+}
+
+int is_prime_number(int target)
+{
+	int upperbound, factor, multiple;
+	int sieve[target + 1];
+	if (target <= 1 )
+		return 0;
+	else
+	{
+		upperbound = _sqrt(target, 2);
+		factor = 2;
+		init_array(sieve, target, 0);
+		return sift_primes(sieve, target, upperbound, factor);
 	}
 }
 
@@ -74,12 +70,40 @@ void init_array(int *array, int max, int pos)
  *
  * Return: int result
  */
-int _sqrt(int guess, int target)
-{
-	int square = guess * guess;
 
-	if (square >= target)
-		return (guess);
-	else
-		return (_sqrt(++guess, target));
-}
+/**
+ * init_array - set all values of given array to 1
+ * @array: pointer to the array to operate on
+ * @max: determines how far in the array to work on
+ * @pos: the current working index 
+ *
+ * Return: void
+ */
+
+/**
+ * find_multiples - traverse an array and set each multiple position of a
+ * factor to 0
+ * @array: the array to operate on
+ * @factor: the factor to find multiples of
+ * @multiple: the current multiple of factor
+ * @stop: the limit in the array to carry the calculations upto
+ *
+ * Return: void
+ */
+
+/**
+ * sift_primes - find all prime positions in given array
+ * @sieve: integer to investigate
+ * @target: integer to investigate
+ * @upperbound: integer to investigate
+ * @factor: integer to investigate
+ *
+ * Return: an int value of 1 for prime or 0 for not prime
+ */
+
+/**
+ * is_prime_number - determine if the given number is a prime
+ * @num: integer to investigate
+ *
+ * Return: an int value of 1 for prime or 0 for not prime
+ */
--- recursion/main.c
+++ recursion/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+void test_func(int num);
+
 int main(void)
 {
+	int i;
+/*
 	char *strA = "Hello, World!";
 	char *strB = "the quick brown fox jumped over the lazy dog";
 
@@ -12,7 +16,24 @@ int main(void)
 
 	printf("function test :\n");
 	printf("the string given is %d long\n", _strlen_recursion(strA));
+*/
+	for (i = 0; i < 50; i++)
+	{
+		test_func(i);
+	}
 
 	return (0);
 }
 
+void test_func(int num)
+{
+	int p = is_prime_number(num);
+	char *result;
+
+	if (p == 1)
+		result = "PRIME";
+	else
+		result = "NOT prime";
+
+	printf("%d is %s\n", num, result);
+}
