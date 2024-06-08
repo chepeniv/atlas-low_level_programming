e1fdd0eb71cca5b2cf0aa6143a5041edd1940628
chepeniv <jose.olmos@atlasschool.com>
2024-06-08 04:21:00 +0000

    initial working logic test passed

--- recursion/_is_prime.c
+++ recursion/_is_prime.c
@@ -1,35 +1,70 @@
 #include "main.h"
+#include <stdio.h>
+#include <limits.h>
 
 int _sqrt(int guess, int target);
+int is_prime(int num);
+void test_func(int num);
 
-int is_prime_number(int num)
+int main(void)
 {
-	int sieve[] = { [0 ... num + 1] = 1 };
-	int upperbound = _sqrt(0, num);
-	int factor = 2;
-	int multiple = 0;
+	test_func(7);
+	return (0);
+}
 
-	while (factor <= upperbound)
-		multiple = factor + factor;
-		while (mutiple <= num + 1)
-			sieve[multiple] = 0;
-			multiple += factor;
-		if (sieve[num + 1] == 0)
-			return 0;		
-		factor++;
+void test_func(int num)
+{
+	int p = is_prime(num);
+	char *result;
 
-	return sieve[num + 1];
+	if (p == 1)
+		result = "PRIME";
+	else
+		result = "NOT prime";
+
+	printf("the given integer %d is %s\n", num, result);
 }
 
 int _sqrt(int guess, int target)
 {
 	int square = guess * guess;
 
-	if (target < 0)
-		return (-1);
-	else if (square >= target)
+	if (square >= target)
 		return (guess);
 	else
-		return (_calc_sqrt(++guess, target));
+		return (_sqrt(++guess, target));
 }
 
+int is_prime(int num)
+{
+	int upperbound, factor, multiple, i;
+	int sieve[num + 1];
+
+	if (num <= 2 )
+		return 0;
+	else
+	{
+		upperbound = _sqrt(2, num);
+		factor = 2;
+		for (i = 0; i <= num; i++)
+			sieve[i] = 1;
+	}
+
+	while (factor <= upperbound)
+	{
+		if (sieve[factor] == 1 )
+		{
+			multiple = factor + factor;
+			while (multiple <= num)
+			{
+				sieve[multiple] = 0;
+				multiple += factor;
+			}
+			if (sieve[num] == 0)
+				return 0;		
+		}
+		factor++;
+	}
+
+	return 1;
+}
