dd7e929022d22bf315f154b246d9eb31d85c32fe
chepeniv <jose.olmos@atlasschool.com>
2024-06-09 17:37:08 +0000

    attempting to prevent mixed-decl error. pst

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,18 +10,19 @@
  */
 int main(int argc, char **argv)
 {
+	int a;
+	int b;
+	int product;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	char *argA = argv[1];
-	char *argB = argv[2];
-	int a = atoi(argA);
-	int b = atoi(argB);
-	int product = a * b;
-
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	product = a * b;
 	printf("%d\n", product);
 
 	return (0);
