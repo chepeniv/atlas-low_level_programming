2c0e87b52e6ad2117d87c788e0f75513110382a3
chepeniv <jose.olmos@atlasschool.com>
2024-06-09 16:59:19 +0000

    added betty docwq

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - a program that outputs its name to stdout
+ * @argc: number of arguments passed
+ * @argv: array of pointers to strings arguments passed 
+ *
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
 	(void) argc;
+
+	printf("%s\n", argv[0]);
 	return (0);
 }
