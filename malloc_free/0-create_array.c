8a93659e057ebd1dfe3eb64e02703d14e2fc76d5
chepeniv <jose.olmos@atlasschool.com>
2024-06-16 14:52:57 -0500

    betty style fixes

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,13 +16,13 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	c_string = malloc(size * sizeof *c_string);
+	c_string = malloc(size * sizeof(*c_string));
 	if (c_string == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; ++i)
 		c_string[i] = c;
 	c_string[size] = '\0';
-	
+
 	return (c_string);
 }
