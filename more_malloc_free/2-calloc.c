55c105a14ea143371fc67841e49bc7e05ea8cbfd
chepeniv <jose.olmos@atlasschool.com>
2024-06-17 12:44:13 -0500

    untested code

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate initialized memory based on item count and size
+ * @items: number of items
+ * @size: number of bytes of each item
+ *
+ * Return: success: void pointer to a location on the heap; failure: NULL
+ */
+void *_calloc(unsigned int items, unsigned int size)
+{
+	int i;
+	void *marray;
+
+	if (items == 0 || size == 0)
+		return (NULL);
+
+	marray = malloc(items * size);
+	if (marray == NULL)
+		return (NULL);
+
+	for (i = 0; i < items; i++)
+		marray[i] = 0;
+
+	return (marray);
+}
