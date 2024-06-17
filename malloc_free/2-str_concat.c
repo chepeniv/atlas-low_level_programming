0f1a21e89e2ccaf1e8865746445db72e436c14d6
chepeniv <jose.olmos@atlasschool.com>
2024-06-16 19:48:51 -0500

    betty doc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
- * _strdup - create a duplicate of a given string upon the heap
- * @original: string to duplicate.
+ * str_concat - concatinate, on the heap, two given strings
+ * @A: prefix string
+ * @B: suffix string
  *
- * Return: pointer to the constructed array upon success, otherwise NULL
+ * Return: pointer to the constructed string upon success, otherwise NULL
  */
 char *str_concat(char *A, char *B)
 {
