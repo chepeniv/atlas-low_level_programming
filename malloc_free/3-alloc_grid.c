dc231496917ff1c09e5dc7872765c16d09a33930
chepeniv <jose.olmos@atlasschool.com>
2024-06-17 01:28:35 -0500

    abandoned alternative approach. figured out how to use git-restore

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
 /**
  * alloc_grid - construct a 2D array of integers initialized to 0
  * @width: array width
- * @heigh: array height
+ * @height: array height
  *
  * Return: success - return pointer to array; failure - return NULL
  */
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 {
 	int *vector;
 	int **matrix;
-	int i;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -21,18 +21,20 @@ int **alloc_grid(int width, int height)
 	if (matrix == NULL)
 		return (NULL);
 
-	vector = malloc(width * sizeof(int));
-	if (vector == NULL)
-	{
-		free(matrix);
-		return (NULL);
-	}
-
-	for (i = 0; i < width; i++)
-		vector[i] = 0;
-
 	for (i = 0; i < height; i++)
+	{
+		vector = malloc(width * sizeof(int));
+		if (vector == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(matrix[j]);
+			free(matrix);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			vector[j] = 0;
 		matrix[i] = vector;
+}
 
 	return (matrix);
 }
