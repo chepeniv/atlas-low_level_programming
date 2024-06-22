2033c3db2cb2cfdb0b32ff303ddcef8634e348f6
chepeniv <jose.olmos@atlasschool.com>
2024-06-22 00:01:08 -0500

    moved struct definition to task file

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,6 +3,19 @@
 #include "main.h"
 
 /**
+ * struct dog - a composite type holding a dog's info
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+/**
  * init_dog - takes a dog struct and initializes it with the
  * given parameters
  * @d: the dog struct to modify
--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -7,16 +7,15 @@ int main(void)
 {
 	typedef struct dog dog;
 	dog adog;
-	dog *to_adog = &adog;
 	char *name ="thomas";
 	char *owner ="eric";
 	float age = 2.4;
 
-	init_dog(to_adog, name, age, owner);
+	init_dog(&adog, name, age, owner);
 
-	printf("name: %s\n", to_adog->name);
-	printf("age: %f\n", to_adog->age);
-	printf("owner: %s\n", to_adog->owner);
+	printf("name: %s\n", adog.name);
+	printf("age: %f\n", adog.age);
+	printf("owner: %s\n", adog.owner);
 
 	return 0;
 }
