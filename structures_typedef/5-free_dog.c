abbf83e509cacd60779af0131aaa84194541b9d0
chepeniv <jose.olmos@atlasschool.com>
2024-06-22 02:09:57 -0500

    created file

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - deallocate memory allocated for a given dog
+ * @d: reference to dog
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
