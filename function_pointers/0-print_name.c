c21dbb6cc19731b051845f56773dcf3e111a1c5e
chepeniv <jose.olmos@atlasschool.com>
2024-06-22 17:31:37 -0500

    added betty doc

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "function_pointers.h"
-
+/**
+ * print_name - a function that callsback another
+ * @name: parameter to passon to the callback
+ * @f: the function to callback
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
