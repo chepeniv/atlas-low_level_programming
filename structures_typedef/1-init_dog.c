5bd3724cff2fd57387b7e260d2c9a48c9e7718ee
chepeniv <jose.olmos@atlasschool.com>
2024-06-21 21:50:04 -0500

    fixed: mixed up type with var

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -15,7 +15,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog->name = name;
-	dog->owner = owner;
-	dog->age = age;
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
 }
