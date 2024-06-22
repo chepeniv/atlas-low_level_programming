98443648eb88ee440ee97189df7d01b2a2170cc5
chepeniv <jose.olmos@atlasschool.com>
2024-06-21 21:36:03 -0500

    added: betty doc

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog - a composite type holding a dog's info
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ */
 struct dog {
 	char *name;
 	char *owner;
