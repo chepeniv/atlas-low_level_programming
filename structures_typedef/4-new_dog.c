b8b3bfb95941d3667a20ba440159e5ef24cc5e79
chepeniv <jose.olmos@atlasschool.com>
2024-06-22 01:45:45 -0500

    added my own _strdup.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,15 +4,23 @@
 
 /**
  * print_dog - takes a dog struct and prints-out its contents
- * @d: the dog struct to reference
+ * @name: the dog struct to reference
+ * @age: the dog struct to reference
+ * @owner: the dog struct to reference
  *
- * Return: void
+ * Return: dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
+	char *dup_name = _strdup(name);
+	char *dup_owner = _strdup(owner);
 
 	if (d == NULL)
+		return (NULL);
+	if (dup_name == NULL)
+		return (NULL);
+	if (dup_name == NULL)
 		return (NULL);
 
 	d->name = name;
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -3,4 +3,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strdup(char *original);
+
 #endif
