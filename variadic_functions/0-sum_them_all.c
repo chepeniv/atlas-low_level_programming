f94e2935da99a730abfcee0729cac96aea40a266
chepeniv <jose.olmos@atlasschool.com>
2024-06-24 18:29:10 -0500

    sandbox untested code

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,2 +1,22 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all of the given arguments after the first
+ * @n: the number of arguments to sum
+ *
+ * Return: the resultant sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i;
+	va_list numbers;
+
+	va_start(numbers, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(numbers, int);
+
+	va_end(numbers);
+	return sum;
+}
