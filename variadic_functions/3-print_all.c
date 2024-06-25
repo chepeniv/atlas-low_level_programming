bb17f4d4a054036df20a5444197623691c2fdc88
chepeniv <jose.olmos@atlasschool.com>
2024-06-24 22:23:07 -0500

    removed else-if slightly modified logic

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <strings.h>
 #include "variadic_functions.h"
 
 /**
@@ -11,13 +12,18 @@
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
-	va_list arglist;
 	char * astr;
+	va_list arglist;
+	int length = 0;
 	int next = 0;
 
 	va_start(arglist, format);
 
 	while (format[i] != '\0')
+		length = i++;
+
+	i = 0;
+	while (i <= length)
 	{
 		switch (format[i])
 		{
@@ -46,14 +52,13 @@ void print_all(const char * const format, ...)
 		}
 
 		i++;
-		if (format[i] == '\0')
-			printf("\n");
-		else if (next)
+		if (i <= length && next)
 		{
 			printf(", ");
 			next = 0;
 		}
 	}
+	printf("\n");
 
 	va_end(arglist);
 }
