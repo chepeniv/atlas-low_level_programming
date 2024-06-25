353e94332702513adb0b3054ad97da6fb632d547
chepeniv <jose.olmos@atlasschool.com>
2024-06-24 22:07:24 -0500

    fixed needless comma printing. ready for checker

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -19,6 +19,7 @@ void print_all(const char * const format, ...)
 	unsigned int i = 0;
 	va_list arglist;
 	char * astr;
+	int next = 0;
 
 	va_start(arglist, format);
 
@@ -28,14 +29,17 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", (char) va_arg(arglist, int));
+				next = 1;
 				break;
 
 			case 'i':
 				printf("%d", va_arg(arglist, int));
+				next = 1;
 				break;
 
 			case 'f':
 				printf("%f", va_arg(arglist, double));
+				next = 1;
 				break;
 
 			case 's':
@@ -43,14 +47,18 @@ void print_all(const char * const format, ...)
 				if (astr == NULL)
 					astr = "(nil)";
 				printf("%s", astr);
+				next = 1;
 				break;
 		}
 
 		i++;
 		if (format[i] == '\0')
 			printf("\n");
-		else
+		else if (next)
+		{
 			printf(", ");
+			next = 0;
+		}
 	}
 
 	va_end(arglist);
