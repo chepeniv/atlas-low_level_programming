8721064b8f1b0d6332a30044462efdf0e02bc61b
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 02:07:30 -0500

    init: singly_linked_lists files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list -
+ * @h: a pointer to the first item in a linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	/* printf is permited */
+	/* return number of nodes */
+	/* print "[0] (nil)" if str == NULL */
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,12 @@
+project : Singly Linked Lists
+
+global variables not allowed\
+no more than five functions per file\
+standard library functions allowed :
+- malloc
+- free
+- exit
+
+\_putchar is allowed\
+include all function signatures in lists.h\
+all header files should be guarded
