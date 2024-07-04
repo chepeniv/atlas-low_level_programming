1d3cf5c268ac12db22010a3507b99713d4befb9e
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 05:24:40 -0500

    [untested] created file

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - measures the length of a linked list
+ * @h: a pointer to the first item
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h);
+{
+	if (h == NULL)
+		return (0);
+
+	if (h->next == NULL)
+		return (1);
+	else
+		return (print_list(h->next) + 1);
+}
