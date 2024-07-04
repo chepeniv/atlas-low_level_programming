82f4e0071b304372fc914268e2db830aacd17e3d
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 18:11:01 -0500

    [untested] created task0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - printout data fields for each element in linked list
+ * @h: a pointer to the first item in a linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h);
+{
+	listint_t *current = h;
+	int total = 0;
+
+	if (h == NULL)
+		return (total);
+
+	while (current->next != NULL)
+	{
+		printf("%d\n", current->n);
+		total++;
+		current = current->next;
+	}
+	printf("%d\n", current->n);
+	total++;
+
+	return (total);
+}
