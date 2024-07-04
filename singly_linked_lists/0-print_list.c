a1f9f09e3ba09808b5f0cf1fbcd24a688872cd4f
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 05:05:43 -0500

    [untested] created: print_list() func

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,14 +3,23 @@
 #include <stdlib.h>
 
 /**
- * print_list -
+ * print_list - printout a data field for each element in linked list
  * @h: a pointer to the first item in a linked list
  *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	/* printf is permited */
-	/* return number of nodes */
-	/* print "[0] (nil)" if str == NULL */
+	if (h == NULL)
+		return (0);
+
+	if (h->str == NULL)
+		printf("[%d] %s\n", h->len, "(nil)");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+
+	if (h->str == NULL)
+		return (1);
+	else
+		return (print_list(h->next) + 1);
 }
