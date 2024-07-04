76cf26b25ce882966dda840eab04a7b3aa2664f1
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 18:22:22 -0500

    [untested] created task2 .c file

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - measures the length of a linked list
+ * @h: a pointer to the first node
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	int total = 0;
+	const listint_t current = h;
+
+	if (current == NULL)
+		return (total);
+	if (current->next == NULL)
+		return (++total);
+	while (current->next != NULL)
+	{
+		total++;
+		current = current->next;
+	}
+
+	return (total);
+}
