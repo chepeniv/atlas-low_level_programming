da6b003774afe56bb1606b7518452fc0e1e2c65c
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 21:43:42 -0500

    [untested] created .c task file

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * get_nodeint_at_index - find and return the nth node
+ * @head: a pointer to the first node
+ * @index: position of the nth node
+ *
+ * Return: the nth node if found otherwise NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	const listint_t *current = head;
+	int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		++i;
+	}
+
+	return (current);
+}
