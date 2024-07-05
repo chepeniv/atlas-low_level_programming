92a25d2defc57eae20d97cc2bbedfd31cb266b5a
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 23:41:05 -0500

    [untested] created file

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * insert_nodeint_at_index - insert a new node in list at index
+ * @index: position in list
+ * @value: data for new node
+ *
+ * Return: pointer to the new node or NULL upon failure
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int value)
+{
+	listint_t *before, *after, *new;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	before = *head;
+	while (before != NULL && i < index - 1)
+	{
+		before = before->next;
+		++i;
+	}
+	if (i != index - 1)
+		return (NULL);
+
+	new = malloc(sizeof(listint_s));
+	if (new == NULL)
+		return (NULL);
+
+	after = before->next;
+	before->next = new;
+	new->next = after;
+	new->n = value;
+
+	return (new);
+}
