16513716f6197cb442447d2ecc6eca43bb183520
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 22:01:53 -0500

    [untested] created task .c file

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * sum_listint - returns the sum of a list node's data
+ * @head: a pointer to current node
+ *
+ * Return: sum of all data
+ */
+int sum_listint(listint_t *current)
+{
+	int sum;
+
+	if (current == NULL)
+		return (0);
+
+	sum = current->n;
+
+	if (current->next == NULL)
+		return (sum);
+
+	return (sum += sum_listint(current->next));
+}
