e8abf5f0bf13c416d8e22f71f214b7d4b0d9121f
chepeniv <jose.olmos@atlasschool.com>
2024-07-05 05:25:06 -0500

    [untested] modified logic

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int val
 	listint_t *before, *after, *new;
 	unsigned int i = 0;
 
-	if (head == NULL || *head == NULL)
+	if ((head == NULL ||
+		*head == NULL) &&
+		index != 0)
 		return (NULL);
 
 	before = *head;
@@ -37,3 +39,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int val
 
 	return (new);
 }
+
+/*
+  listint_t *get_nodeint_at_index(*head, index);
+  size_t listint_len(*head);
+  listint_t *add_nodeint(**pos, n);
+*/
