f4313bc1e2de9984e946e844a057ea90abd57a9c
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 21:28:06 -0500

    [untested] created plausible sourcecode

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - delete head node
+ * @head: a double pointer to the head node
+ *
+ * Return: head node's int value
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int nodedata;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	temp = *head;
+	nodedata = temp->n;
+	*head = temp->next;
+	free(temp);
+
+	return(nodedata);
+}
