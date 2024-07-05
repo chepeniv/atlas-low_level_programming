949c758549937417fe90d7ecc66de694d90d586b
chepeniv <jose.olmos@atlasschool.com>
2024-07-04 20:55:24 -0500

    [untested] created task .c file

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint - destroys a linked list
+ * @head: a pointer to the head node
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	list_t *current;
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+
+	current = head;
+	head = NULL;
+	while (current->next != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	free(current);
+}
