49277c582103e8db8472d7a1b797ca5bf5eba1b8
chepeniv <jose.olmos@atlasschool.com>
2024-07-06 05:05:19 -0500

    [passing] created file

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <limits.h>
+
+ulint power_of_two(ulint power);
+
+/**
+ * print_binary - output the binary representation of a given int
+ * @n: number to translate
+ *
+ * Return: void
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int result;
+	ulint pot;
+
+	pot = power_of_two(index);
+	result = n & pot;
+
+	if (result)
+		result = 1;
+
+	return (result);
+}
+
+ulint power_of_two(ulint power)
+{
+	ulint result = 1;
+
+	while (power)
+	{
+		result *= 2;
+		--power;
+	}
+
+	return (result);
+}
