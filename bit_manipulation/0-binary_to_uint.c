eff452bbebe6770fd1e8b371e1b0b2f3824f4c82
chepeniv <jose.olmos@atlasschool.com>
2024-07-05 21:06:18 -0500

    [passing] init project dir, created task0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - translates binary code into positive decimals integers
+ * @b: string consisting of binary digits
+ *
+ * Return: the decimal conversion, or 0 upon failure
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len = 0;
+	unsigned int pos = 0;
+	unsigned int sum = 0;
+
+	while (b[len] != '\0')
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		len++;
+	}
+
+	while (pos < len)
+	{
+		if (b[pos] == '1')
+			sum += 1 << (len - pos - 1);
+		pos++;
+	}
+	return (sum);
+}
---
+++ bit_manipulation/README.md
@@ -0,0 +1,12 @@
+project : Bit Manipulation
+
+global variables not allowed\
+no more than five functions per file\
+standard library functions allowed :
+- malloc
+- free
+- exit
+
+\_putchar is allowed\
+include all function signatures in main.h\
+all header files should be guarded
