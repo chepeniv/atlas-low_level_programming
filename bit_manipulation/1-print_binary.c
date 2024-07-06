d2c0a54e547855535aaf8d7e26619fbd9cb6eef4
chepeniv <jose.olmos@atlasschool.com>
2024-07-05 22:56:26 -0500

    [passing] created file

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - output the binary representation of a given int
+ * @n: number to translate
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int sum = 0;
+	int power = 0;
+
+	if (n == 1 || n == 0)
+	{
+		_putchar(n + '0');
+		return;
+	}
+
+	while (sum < n)
+	{
+		sum = 1 << power;
+		power++;
+	}
+
+	if (sum > n)
+	{
+		power--;
+		sum = 1 << power;
+	}
+
+	_putchar('1');
+	power--;
+	while (power > -1)
+	{
+		if (sum > n)
+		{
+			_putchar('0');
+			sum -= 1 << power;
+		}
+		else
+			_putchar('1');
+		power--;
+		sum += 1 << power;
+	}
+}
