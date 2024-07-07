83752e0d3f3890358bf8e31f3873adafe8920b6b
chepeniv <jose.olmos@atlasschool.com>
2024-07-07 10:16:50 -0500

    [untested] reverted change - no effect

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,9 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	desc = open(filename, O_RDWR | O_TRUNC);
-	if (desc < 0)
-		desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	desc = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (desc < 0)
 		return (-1);
 
