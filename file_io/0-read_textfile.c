d5e303515177f2ce70d2a14048ef7e94f3665b35
chepeniv <jose.olmos@atlasschool.com>
2024-07-07 09:01:05 -0500

    [untested] added malloc; removed needless compare

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -18,15 +18,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int desc;
 	char *buffer;
 
-	if (filename == NULL || total > letters || total == 0)
+	if (filename == NULL || letters == 0)
 		return (0);
 
 	desc = open(filename, O_RDONLY);
 	if (desc < 0)
 		return (0);
-
+	buffer = malloc(sizeof(char)*letters);
 	total = read(desc, buffer, letters);
 	total = write(STDOUT_FILENO, buffer, total);
+	free(buffer);
 
 	if (total < 0)
 		return (0);
