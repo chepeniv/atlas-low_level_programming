275c3fc91fd0b7f6c16234bf09b76cf444d9f200
chepeniv <jose.olmos@atlasschool.com>
2024-07-11 01:04:35 -0500

    [untested] created task file

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums the data of all the nodes in a given list
+ * @head: pointer to head of the list
+ *
+ * Return: the resultant int; otherwise 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *current = head;
+
+	if (current == NULL)
+		return (0);
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (current);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,6 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+int sum_dlistint(dlistint_t *head);
 void free_dlistint(dlistint_t *head);
 
 #endif
