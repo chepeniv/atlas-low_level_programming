f8a2864e2d208a37b55be202effc676abc6e1ba2
chepeniv <jose.olmos@atlasschool.com>
2024-07-11 10:57:18 -0500

    [untested] removed residual ;

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -6,7 +6,7 @@
  *
  * Return: generated hash
  */
-unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
