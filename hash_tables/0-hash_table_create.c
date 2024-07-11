3e9d89ffe87578d33e97b138d8b91e6d0c1c7c0f
chepeniv <jose.olmos@atlasschool.com>
2024-07-11 10:47:12 -0500

    [untested] added free() after malloc fail

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,7 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
+	{
+		free(htarray);
 		return (NULL);
+	}
 	ht->size = size;
 	ht->array = &htarray;
 
