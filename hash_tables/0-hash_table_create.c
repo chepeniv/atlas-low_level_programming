66af0c1b16f02004d428fc84d3ff9e6a0466537e
chepeniv <jose.olmos@atlasschool.com>
2024-07-11 10:31:52 -0500

    [untested] init task

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,14 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -
- * @size:
+ * hash_table_create - attemp to create a hash table
+ * @size: size of the hash table's node array
  *
  * Return:
+ * success,address to the newly created hashtable;
+ * failure, NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t newht;
+	hash_table_t *ht;
+	hash_node_t *htarray;
 
-	return (newht);
-}
+	htarray = malloc(sizeof(hash_node_t) * size);
+	if (htarray == NULL)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t))
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = &htarray;
+
+	return (ht);
