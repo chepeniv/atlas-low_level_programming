3e654ca9e3be8fb00bf1e9f5eced1b2b3e67fa4e
chepeniv <jose.olmos@atlasschool.com>
2024-07-10 22:35:08 -0500

    [untested] init project dir; created task0

---
+++ doubly_linked_list/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - printout the data of each element in doubly linked list
+ * @h: a pointer to the current items in the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	if (h->next == NULL)
+		return (1);
+
+	printf("&d\n", h->n);
+
+	else
+		return (print_list(h->next) + 1);
+}
---
+++ doubly_linked_list/README.md
@@ -0,0 +1,13 @@
+#project : Doubly Linked List
+
+global variables not allowed\
+no more than five functions per file\
+standard library functions allowed :
+- malloc
+- free
+- printf
+- exit
+
+\_putchar is allowed\
+include all function signatures in lists.h\
+all header files should be guarded
