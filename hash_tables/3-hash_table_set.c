ce0c31d127f28f710ed4d7e3afbeb77b1cb8f839
chepeniv <jose.olmos@atlasschool.com>
2024-07-11 13:20:09 -0500

    [tested] refined logic somewhat

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,20 +10,19 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, size;
+	unsigned long int index;
 	char *valdup, *keydup;
-	hash_node_t **array, **pos, *new;
+	hash_node_t *array, *pos, *new;
 
 	if (key == NULL)
 		return (0);
 
-	size = ht->size;
-	index = key_index((unsigned char *) key, size);
+	index = key_index((unsigned char *) key, ht->size);
 
-	valdup = strdup(value);
 	keydup = strdup(key);
+	valdup = strdup(value);
 
-	array = ht->array;
+	array = *(ht->array);
 	pos = array + index;
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
@@ -31,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->key = keydup;
 	new->value = valdup;
 	new->next = NULL;
-	*pos = new;
+	*pos = *new;
 
 	return (1);
 }
