8ff5985cdfd26fa8b5a47d6190fd48838610a628
chepeniv <jose.olmos@atlasschool.com>
2024-07-11 07:01:38 -0500

    [passing] created .c .test.c files

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - finds the address of a node at a given index within
+ * a doubly linked list
+ * @head: pointer to head of the list
+ * @index: node to find
+ *
+ * Return: address of node if found, NULL otherwise
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *before = NULL, *new = NULL, *after = NULL;
+
+	if (*head == NULL)
+		return (NULL);
+
+	after = *head;
+	while (after != NULL && i < index)
+	{
+		before = after;
+		after = after->next;
+		++i;
+	}
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	before->next = new;
+	new->prev = before;
+	new->next = after;
+	after->prev = new;
+
+	return (new);
+}
