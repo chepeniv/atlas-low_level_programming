558051a1e77d4dbcb81d97b728e2676b407f8a76
chepeniv <jose.olmos@atlasschool.com>
2024-07-11 12:37:50 -0500

    [untested] removed signedness from keydup var

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, size;
-	unsigned char *valdup, *keydup;
+	char *valdup, *keydup;
 	hash_node_t **array, **pos, *new;
 
 	if (key == NULL)
