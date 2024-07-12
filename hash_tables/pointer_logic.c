89b3725a8bc6888756792a4dfcedda0799a9996c
chepeniv <jose.olmos@atlasschool.com>
2024-07-12 03:34:45 -0500

    [tested] starting work session backup

--- hash_tables/pointer_logic.c
+++ hash_tables/pointer_logic.c
@@ -1,18 +1,20 @@
 #include "hash_tables.h"
 
-/**
- * hash_table_set -
- * @ht:
- * @key:
- * @value:
- *
- * Return: success, 1; failure, 0
- */
+typedef struct _numnode {
+	int num;
+} numnode;
+
+int main(void)
+{
+	numnode **array, *newnode;
+
+}
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	char *valdup, *keydup;
-	hash_node_t *array, *new, *pos;
+	hash_node_t *array, *new;
 
 	if (key == NULL)
 		return (0);
@@ -23,16 +25,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	valdup = strdup(value);
 
 	array = *(ht->array);
-	pos = array + index;
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	array[index] = malloc(sizeof(hash_node_t));
+	if (array[index] == NULL)
 		return (0);
-	new->key = keydup;
-	new->value = valdup;
-	new->next = NULL;
-	pos = new;
-	if (pos == NULL)
-		return (0);
+	array[index]->key = keydup;
+	array[index]->value = valdup;
+	array[index]->next = NULL;
 
 	return (1);
 }
