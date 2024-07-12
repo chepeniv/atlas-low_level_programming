2a72dfa3d2ecfe7d78057eca0ac8fff6e175caf9
chepeniv <jose.olmos@atlasschool.com>
2024-07-12 05:58:54 -0500

    [tested] wrangling with pointer logic

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,27 +12,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	char *valdup, *keydup;
-	hash_node_t *array, *new, *pos;
+	hash_node_t **array, **pos, *new;
 
 	if (key == NULL)
 		return (0);
 
 	index = key_index((unsigned char *) key, ht->size);
-
 	keydup = strdup(key);
 	valdup = strdup(value);
 
-	array = *(ht->array);
-	pos = array + index;
+	array = ht->array;
+	pos = &array[index];
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
 	new->key = keydup;
 	new->value = valdup;
 	new->next = NULL;
-	pos = new;
-	if (pos == NULL)
-		return (0);
+	*pos = new;
 
 	return (1);
 }
