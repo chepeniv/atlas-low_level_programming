6f6a2fd5d47e280835e7c6ffbd257d450961a4e2
chepeniv <jose.olmos@atlasschool.com>
2024-07-12 13:54:21 -0500

    scraping it. starting over

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,29 +10,5 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	char *keydup, *valdup;
-	hash_node_t **array, *first, *pos, *new;
-
-	if (key == NULL)
-		return (0);
-
-	index = key_index((unsigned char *) key, ht->size);
-
-	keydup = strdup(key);
-	valdup = strdup(value);
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-	new->key = keydup;
-	new->value = valdup;
-	new->next = NULL;
-
-	array = ht->array;
-	first = *array;
-	pos = first + index;
-	pos = &*new;
-
 	return (1);
 }
