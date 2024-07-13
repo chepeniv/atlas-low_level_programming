07b6dab4c43879ae169fd1571ec393042fa3057f
chepeniv <jose.olmos@atlasschool.com>
2024-07-13 14:43:43 -0500

    [tested] alternative assignment method?

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	pos = ht->array + index;
 	new = malloc(sizeof(hash_node_t));
 
-	*pos = new;
+	pos = &new;
 	(*pos)->key = keydup;
 	(*pos)->value = valdup;
 	(*pos)->next = NULL;
