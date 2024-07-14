bec070f4b40865019b9aca73ffd651077ae82830
chepeniv <jose.olmos@atlasschool.com>
2024-07-14 11:30:17 -0500

    [untested] typo

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -22,7 +22,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	while (strcmp(node->key, key))
 		node = node->next;
-	if (node-value != NULL)
+	if (node->value != NULL)
 		strdata = node->value;
 	else
 		strdata = "(null)";
