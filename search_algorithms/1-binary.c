11003ab9164db3021012efeb402257e3f07fef89
chepeniv <jose.olmos@atlasschool.com>
2024-08-08 10:55:15 -0500

    [failing] saving progress

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 void print_progress(int *array, int pos, int len);
-int binary_search_eng(int *array, int pos, int len, int value);
+int binary_search_eng(int *array, int cap, int pos, int len, int value);
 
 /**
  * binary_search - searches through a sorted array of numbers until it finds
@@ -14,53 +14,28 @@ int binary_search_eng(int *array, int pos, int len, int value);
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int pos, len;
-
 	if (array == NULL)
 		return (-1);
 
-	len = size - 1;
-	pos = len / 2;
-	/*print_progress(array, 0, len);*/
-
-	printf("RANGE %d - %d\n", 0, len);
-	if (value == array[pos])
-		return (pos);
-	else if (value > array[pos])
-	{
-		printf("GREATER THAN %d\n", array[pos]);
-		printf("----------------\n");
-		return binary_search_eng(array, pos, len, value);
-	}
-	else
-	{
-		printf("LESS THAN %d\n", array[pos]);
-		printf("----------------\n");
-		return binary_search_eng(array, 0, len, value);
-	}
+	return binary_search_eng(array, size - 1, 0, size - 1, value);
 }
 
-int binary_search_eng(int *array, int pos, int len, int value)
+int binary_search_eng(int *array, int cap, int pos, int len, int value)
 {
-	if (len == 0 && pos != 0)
+	if ((len == 0 && pos != 0) || pos > cap)
 		return (-1);
 
-	printf("RANGE %d - %d\n", pos, pos + len);
+	print_progress(array, pos, len);
+	getchar();
+
 	len = len / 2;
-	/*print_progress(array, pos, len);*/
 
 	if (value == array[pos + len])
 		return (pos + len);
 	else if (value > array[pos + len])
-	{
-		printf("GREATER THAN %d\n", array[pos + len]);
-		return binary_search_eng(array, pos + len + 1, len, value);
-	}
+		return binary_search_eng(array, cap, pos + len + 1, len, value);
 	else
-	{
-		printf("LESS THAN %d\n", array[pos + len]);
-		return binary_search_eng(array, pos, len - 1, value);
-	}
+		return binary_search_eng(array, cap, pos, len - 1, value);
 }
 
 /**
