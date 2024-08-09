b28636b9dda7f65ce021a7c60c12c448cf4c55cc
chepeniv <jose.olmos@atlasschool.com>
2024-08-09 02:48:10 -0500

    [passing] completed implementation

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,8 +1,5 @@
 #include "search_algos.h"
 
-void print_progress(int *array, int pos, int len);
-int binary_search_eng(int *array, int cap, int pos, int len, int value);
-
 /**
  * binary_search - searches through a sorted array of numbers until it finds
  * the index of the value given
@@ -17,25 +14,43 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	return binary_search_eng(array, size - 1, 0, size - 1, value);
+	return binary_search_eng(array, 0, size - 1, value);
 }
 
-int binary_search_eng(int *array, int cap, int pos, int len, int value)
+/**
+ * binary_search_eng - the algorithm that powers the binary_search() function
+ * @array: the subarray to search through
+ * @pos: the position from which to start search
+ * @len: length of the subarray to analyse through
+ * @value: the target value
+ *
+ * Return: void
+ */
+int binary_search_eng(int *array, int pos, int len, int value)
 {
-	if ((len == 0 && pos != 0) || pos > cap)
+	int half, mid, up_len, up_pos, low_len, low_pos;
+
+	if (len < 0)
 		return (-1);
 
 	print_progress(array, pos, len);
-	getchar();
 
-	len = len / 2;
-
-	if (value == array[pos + len])
-		return (pos + len);
-	else if (value > array[pos + len])
-		return binary_search_eng(array, cap, pos + len + 1, len, value);
+	half = len / 2;
+	mid = pos + half;
+	low_pos = pos;
+	up_pos = low_pos + half + 1;
+	low_len = half - 1;
+	if (len % 2 == 0)
+		up_len = low_len;
 	else
-		return binary_search_eng(array, cap, pos, len - 1, value);
+		up_len = low_len + 1;
+
+	if (value == array[mid])
+		return (mid);
+	else if (value > array[mid])
+		return binary_search_eng(array, up_pos, up_len, value);
+	else
+		return binary_search_eng(array, low_pos, low_len, value);
 }
 
 /**
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,4 +6,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+void print_progress(int *array, int pos, int len);
+int binary_search_eng(int *array, int pos, int len, int value);
+
 #endif
